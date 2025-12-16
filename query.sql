ebc102a7aaa93b70e1ab469246c4356f01ef2502
Arifur999 <arr96777@gmail.com>
2025-12-16 22:21:14 +0600

    added filter= operation

--- query.sql
+++ query.sql
@@ -11,3 +11,12 @@ SELECT DISTINCT country FROM friends;
 
 SELECT*  FROM friends
 WHERE country = 'USA'
+
+--------------
+SELECT first_name,age,course,greade FROM friends
+WHERE greade = 'A'
+
+---------------------------
+
+SELECT * FROM friends
+WHERE greade = 'A+'
