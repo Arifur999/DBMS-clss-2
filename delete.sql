8ab20da7e770e39c6dfc70708ac6b0b359a731e9
Arifur999 <arr96777@gmail.com>
2025-12-19 19:58:19 +0600

    added DELETE

---
+++ delete.sql
@@ -0,0 +1,7 @@
+-------------DELETE--------------
+
+SELECT * FROM books
+
+DELETE FROM books WHERE book_id = 3  --JA ICCHA CONDITION DEYA JAY
+DELETE FROM books WHERE price >11 AND stock_quantity = 0  --JA ICCHA CONDITION DEYA JAY
+DELETE FROM books WHERE book_id = 3  --JA ICCHA CONDITION DEYA JAY
