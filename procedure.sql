78d748861152ff1cd8289b10b56dfb82adffb020
Arifur999 <arr96777@gmail.com>
2025-12-21 19:07:28 +0600

    added PROCEDURE AND TASK

--- procedure.sql
+++ procedure.sql
@@ -10,3 +10,27 @@ DELETE FROM employees WHERE id = id;
   $$
 
 CALL delete_emp_byid(3)
+
+
+
+
+
+
+CREATE PROCEDURE increase_low_salary(department_name VARCHAR(50))
+
+LANGUAGE PLPGSQL AS 
+$$
+DECLARE 
+  avg_salary INT;
+  BEGIN
+  SELECT AVG(salary)INTO avg_salary FROM employees
+  WHERE department_name = department_name;
+
+
+
+  UPDATE employees SET salary =salary *1.1 
+  WHERE department_name =department_name AND salary < avg_salary;
+  END;
+$$
+
+CALL increase_low_salary('HR')
